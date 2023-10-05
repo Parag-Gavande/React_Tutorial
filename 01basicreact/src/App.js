3d5fa09d8319454efcb7c6b0f841890f4b316327
Parag-Gavande <gavandeparagrcpit@gmail.com>
2023-10-05 07:14:13 +0530

    This is first basic program on react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>Parag Gavande,This is a first react program</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  
+  return (
+    <h1>Parag Gavande ,THis is a basic program on react using vite </h1>
+  )
+}
+
+export default App
